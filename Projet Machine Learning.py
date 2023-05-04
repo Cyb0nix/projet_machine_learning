6ac4dadca228040f53aa6c606dd88da241242f45
Arthur <arrhur.buisson@gmail.com>
2023-05-04 20:41:47 +0200

    add Péparation des données

--- Projet Machine Learning.py
+++ Projet Machine Learning.py
@@ -1,11 +1,11 @@
-#Régression linaire simple
-
 # Importation des librairies
 import numpy as np #linear algebra
 import pandas as pd #data manipulation and analysis
 import seaborn as sns #data visualization
+import matplotlib as matplotlib #data visualization
 import matplotlib.pyplot as plt #data visualization
-import sklearn.preprocessing as skp #machine learning (preprocessing)
+matplotlib.use('TkAgg')
+from sklearn.preprocessing import MinMaxScaler #data normalization
 import sklearn.cluster as skc #machine learning (clustering)
 import warnings # ignore warnings
 warnings.filterwarnings('ignore')
@@ -13,18 +13,62 @@ warnings.filterwarnings('ignore')
 # Importation du dataset
 df = pd.read_csv('C:\Arthur\Efrei Paris\L3\Semestre 6\Intro apprentissage machine\Projet\projet_machine_learning\Data\Data_X.csv')
 
-#affiche les statistiques du dataset
-# print(df.describe()) 
 
-#affiche le nombre de valeur null par colonne
-# print("############# Nbr valeur null ############# \n", df.isnull().sum()) 
+####################################
+###### Péparation des données ######
+####################################
 
+# Affichage des statistiques du dataset
+print("Statistique de df : ", df.describe()) 
+
+# Affichage du nombre de valeur null par colonne
+print("\nNbr valeur null : \n", df.isnull().sum()) 
+
+#supprimer les lignes avec des valeurs 'DE' dans la colonne 'COUNTRY' ainsi que la colonne 'COUNTRY'
+df1 = df[df.COUNTRY != 'DE']
+df.drop(['COUNTRY'], axis=1, inplace=True)
 
 #remplace les valeurs null par la moyenne de la colonne
 df_fill = df.fillna(df.mean())
-# print(df_fill.describe())
+
+#Normalisation des données
+scaler = MinMaxScaler() 
+for element in df_fill.columns:
+     if element != 'ID':
+         df_fill[element] = scaler.fit_transform(df_fill[[element]])
+
+print("\n\nDatas normalisées : \n", df_fill)
 
 
+
+##################################################
+###### Analyse exploratoire des donnees  #########
+##################################################
+
+###### APERCU DES VARIABLES ######
+
+print("Distribution, plage de valeurs et signification des variables : \n")
+print(df_fill.describe())
+
+print("\n\nType des variable : \n")
+print(df_fill.info())
+
+
+###### RELATION ENTRE LES VARIABLES AVEC DES GRAPHIQUES ######
+
+# Histogramme
+
+
+
+# Diagramme en boite
+
+
+
+# Graphiques de dispersion
+
+
+
+####### MATRICE DE CORRELATION #######
 # Calcul de la matrice de corrélation
 correlation_metrics=df_fill.corr() 
 
@@ -47,7 +91,10 @@ sns.heatmap(correlation_metrics, cmap='RdBu',
             cbar_kws={"shrink": .70}, mask=mask)
 
 plt.title('Correlation Between Variables', size=14) 
-plt.show()
+# plt.show()
+
+
+
 
 
 # plt.scatter(df['FR_TEMP'], df['DE_TEMP'], color = 'red')
@@ -55,6 +102,9 @@ plt.show()
 # plt.xlabel('température FR')
 # plt.ylabel('température DE')
 # plt.show()
+
+
+
 
 
 
