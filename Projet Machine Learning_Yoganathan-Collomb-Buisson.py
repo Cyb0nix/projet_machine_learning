9719a0678090fd357071ff12c6dd7f4206e9e92d
Cyb0nix <116296259+Cyb0nix@users.noreply.github.com>
2023-05-09 10:13:57 +0200

    fix path

--- Projet Machine Learning_Yoganathan-Collomb-Buisson.py
+++ Projet Machine Learning_Yoganathan-Collomb-Buisson.py
@@ -13,8 +13,8 @@ import sklearn.cluster as skc #machine learning (clustering)
 
 
 # Importation du dataset
-df = pd.read_csv('\Data\Data_X.csv')
-Y = pd.read_csv('\Data\Data_Y.csv')
+df = pd.read_csv('Data\Data_X.csv')
+Y = pd.read_csv('Data\Data_Y.csv')
 
 
 ####################################
